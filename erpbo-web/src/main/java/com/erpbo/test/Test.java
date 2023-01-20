7c58efa496d062f524fcb6ab566ecf9ba5c51ea9
Ivan Wen <ivan.wb@gmail.com>
2023-01-20 22:23:09 +0800

    测试mavan 多模块开发

---
+++ erpbo-web/src/main/java/com/erpbo/test/Test.java
@@ -0,0 +1,10 @@
+package com.erpbo.test;
+
+import com.erpbp.framework.App;
+
+public class Test {
+    public static  void main(String[] arge){
+        String info= App.getVersion("aaa");
+        System.out.println(info);
+    }
+}
---
+++ hj-core/src/main/java/com/erpbp/framework/App.java
@@ -0,0 +1,17 @@
+package com.erpbp.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String getVersion(String code){
+        System.out.println("aaaaaa-getVersion");
+        return  "myhis---"+code;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
